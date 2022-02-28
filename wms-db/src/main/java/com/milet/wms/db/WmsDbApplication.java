97be044c90ed332140d93ecdb71a358f5ef9811f
hyfly233 <1274694743@qq.com>
2022-02-28 20:45:39 +0800

    commit

---
+++ wms-db/src/main/java/com/milet/wms/db/WmsDbApplication.java
@@ -0,0 +1,13 @@
+package com.milet.wms.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsDbApplication.class, args);
+    }
+
+}
---
+++ wms-db/src/test/java/com/milet/wms/db/WmsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milet.wms.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
